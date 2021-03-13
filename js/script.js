c0cd75ab27f934ccce614775d329f68586ec1008
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-13 18:12:59 +0100

    Aggiunte funzioni per passare all'immagine successiva o precedente

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
+$(".fa-angle-right").click(
+  function() {
+    var imgActive = $('.images img.active');
 
+    if (($('.images img.active')).is('.last')) {
+      $(imgActive).removeClass("active");
+      $('.images img.first').addClass("active");
+    } else {
+      $(imgActive).removeClass("active");
+      $(imgActive).next().addClass("active");
+    }
+
+  }
+);
+
+$(".fa-angle-left").click(
+  function() {
+    var imgActive = $('.images img.active');
+
+    if (($('.images img.active')).is('.first')) {
+      $(imgActive).removeClass("active");
+      $('.images img.last').addClass("active");
+    } else {
+      $(imgActive).removeClass("active");
+      $(imgActive).prev().addClass("active");
+    }
+
+  }
+);
