f4070c59b834fa7ae1c8ae4fdef38bb56bcf9f43
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-13 18:17:39 +0100

    Aggiunto anche il movimento dei bullet blu alle due funzioni

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 $(".fa-angle-right").click(
   function() {
     var imgActive = $('.images img.active');
+    var circleActive = $('.nav i.active');
 
     if (($('.images img.active')).is('.last')) {
       $(imgActive).removeClass("active");
       $('.images img.first').addClass("active");
+      $(circleActive).removeClass("active");
+      $('.nav i.first').addClass("active");
     } else {
       $(imgActive).removeClass("active");
       $(imgActive).next().addClass("active");
+      $(circleActive).removeClass("active");
+      $(circleActive).next().addClass("active");
     }
 
   }
@@ -16,13 +21,18 @@ $(".fa-angle-right").click(
 $(".fa-angle-left").click(
   function() {
     var imgActive = $('.images img.active');
+    var circleActive = $('.nav i.active');
 
     if (($('.images img.active')).is('.first')) {
       $(imgActive).removeClass("active");
       $('.images img.last').addClass("active");
+      $(circleActive).removeClass("active");
+      $('.nav i.last').addClass("active");
     } else {
       $(imgActive).removeClass("active");
       $(imgActive).prev().addClass("active");
+      $(circleActive).removeClass("active");
+      $(circleActive).prev().addClass("active");
     }
 
   }
